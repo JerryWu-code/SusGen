0d1207d942968945210f0d8802beaf31d0526ab4
Jerry Wu <953855668@qq.com>
2024-02-21 18:01:59 +0800

    Modify the extractText_sus.py, tcfd_new.csv and add some extract examples.

--- src/config.py
+++ src/config.py
@@ -2,9 +2,16 @@
 # Institute: National University of Singapore, A Star IHPC
 # Description: Put some global variables (e.g. path, etc.) here.
 
+import os
 #############################################################################
+# Path for downloading the raw PDFs
 raw_data_path = "data/raw_data" # The path to the raw PDFs
-tcfd_list = "data/target_list/tcfd.csv" # The path to the original TCFD list
-esg_list = "data/target_list/esg.csv" # The path to the original ESG list
-updated_tcfd_list = "data/target_list/tcfd_new.csv" # The path to the updated TCFD list
-updated_esg_list = "data/target_list/esg_new.csv" # The path to the updated ESG list
+list_path = "data/target_list" # The path to the target list
+tcfd_list = os.path.join(list_path, "tcfd.csv") # The path to the original TCFD list
+esg_list = os.path.join(list_path, "esg.csv") # The path to the original ESG list
+updated_tcfd_list = os.path.join(list_path, "tcfd_new.csv") # The path to the updated TCFD list
+updated_esg_list = os.path.join(list_path, "esg_new.csv") # The path to the updated ESG list
+
+#############################################################################
+# Path for the extracted pdf & text
+extract_path = os.path.join("data/examples/extractText_sus", "")# The path to the extract part
