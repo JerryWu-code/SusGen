0f145e9efb51d9f923c08585020ecdc9866cbdf7
Jerry Wu <953855668@qq.com>
2024-03-08 22:58:31 +0800

    Add test.py for practice with prompt.

--- llama/inference.sh
+++ llama/inference.sh
@@ -1,4 +1,4 @@
-torchrun --nproc_per_node 1 example_text_completion.py \
+torchrun --nproc_per_node 1 test.py \
     --ckpt_dir llama-2-7b/ \
     --tokenizer_path tokenizer.model \
-    --max_seq_len 512 --max_batch_size 6
+    --max_seq_len 2048 --max_batch_size 8
