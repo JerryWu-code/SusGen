4ab014c2a6d125f53e1f193bfc2d6319442ea9a4
Jerry Wu <953855668@qq.com>
2024-03-17 18:04:44 +0800

    update

--- classification/merge.py
+++ classification/merge.py
@@ -12,7 +12,6 @@ def get_final_csv(file1, file2, output_file):
         file2: the path of zero_predict.csv
         output_file: path of the output the final csv
     """
-
     if not os.path.exists(file1):
         raise FileNotFoundError(f"File '{file1}' not found.")
     if not os.path.exists(file2):
@@ -20,15 +19,14 @@ def get_final_csv(file1, file2, output_file):
 
     # load the csv
     df1 = pd.read_csv(file1)
-    df2 = pd.read_csv(file2, usecols=["text", "predict"]).rename(columns={"predict": "label"})
+    df2 = pd.read_csv(file2, usecols=["text", "predictions"]).rename(columns={"predictions": "label"})
 
     # save final
     merged_df = pd.concat([df1, df2], ignore_index=True)
     merged_df.to_csv(output_file, index=False) 
+    print(f"Successully output file in {output_file}")
 
 if __name__ == "__main__":
     file1, file2 = "./data/non_zero.csv", "./data/zero_predict.csv"
     output_file = "./data/final_tcfd_recommendations.csv"
     get_final_csv(file1, file2, output_file)
-
-    
