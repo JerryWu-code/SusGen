d72e077f1de71bda6702e84d9a7a5692060c4acc
Jerry Wu <953855668@qq.com>
2024-03-24 21:53:40 +0800

    save the de-privacy tcfd_qa data and start synthesize data in parallel.

--- src/llms/mistral-hf/utils/process.py
+++ src/llms/mistral-hf/utils/process.py
@@ -5,7 +5,7 @@
 #    2. Contruct additive diversity version of the dataset.
 
 #############################################################################
-# CUDA_VISIBLE_DEVICES=0 python utils/process.py
+# cd /home/whatx/SusGen/src/llms/mistral-hf && CUDA_VISIBLE_DEVICES=0 python utils/process.py
 import json, os, sys, time
 from tqdm import tqdm
 sys.path.append((os.path.dirname(os.path.dirname(__file__))))
@@ -35,20 +35,23 @@ def append_to_jsonl(data, file_path):
             json.dump(data, file)
             file.write('\n')
 
-def denoise_text(text):
+def denoise_text():
     data = load_json("../../../data/susgen/tcfd_qa/tcfd_qa_v1.json")
     final = []
-    for num, v in tqdm(enumerate(data)):
-        out = v['output']
+    noise = 0
+    for _, v in tqdm(enumerate(data)):
+        out = v['answer']
         # if out includes "\n\n", replace it with "\n"
         if "\n\n" in out:
             out = out.replace("\n\n", "\n")
+            noise += 1
         temp = {
             "instruction": v['instruction'],
             "input": "",
-            "answer": out
+            "output": out
         }
         final.append(temp)
+    print(f"Total noise removed: {noise}")
     save_json(final, "../../../data/susgen/tcfd_qa/tcfd_qa_v1_clean.json")
 
 def task1(model, tokenizer, device, args):
@@ -60,9 +63,9 @@ def task1(model, tokenizer, device, args):
         out = v['output']
         prompt = instr_prompt(
             "Process the text following the instructions below:\n"
-            "1.Replace all the specific company entity name with \"we\" or \"our company\""
+            "1.Replace all the specific company entity name with \"we\" or \"our company\"\n"
             "2.Replace other private information with generic terms\n"
-            f"\nText:\n{out}")
+            f"Text:\n{out}")
         _, answer = generate_text(model, tokenizer, device, prompt=prompt, args=args)
         temp = {
             "instruction": instr,
@@ -77,7 +80,7 @@ def task1(model, tokenizer, device, args):
     save_json(final, "../../../data/susgen/tcfd_qa/tcfd_qa_v1.json")
 
 def task2(model, tokenizer, device, args, times):
-    data = load_jsonl("../../../data/susgen/tcfd_qa/tcfd_qa_v1_clean.json")
+    data = load_json("../../../data/susgen/tcfd_qa/tcfd_qa_v1_clean.json")
     print(f"Total new records will be created: {len(data) * times}")
     state = 0
     final = []
@@ -90,12 +93,12 @@ def task2(model, tokenizer, device, args, times):
                 "1.Rephrase the whole text without change original meaning and elements.\n"
                 "2.Adjust the processed text to similar length as the original text.\n"
                 "3.Ensure the text is coherent and fluent and output the final text.\n"
-                f"\nText:\n{instr}")
+                f"Text:\n{instr}")
             prompt2 = instr_prompt(
                 "Process the text following the instructions below:\n"
-                "1.Replace all the specific company entity name with \"we\" or \"our company\""
+                "1.Replace all the specific company entity name with \"we\" or \"our company\"\n"
                 "2.Replace other private information with generic terms\n"
-                f"\nText:\n{out}")
+                f"Text:\n{out}")
             _, answer1 = generate_text(model, tokenizer, device, prompt=prompt1, args=args)
             _, answer2 = generate_text(model, tokenizer, device, prompt=prompt2, args=args)
             temp = {
@@ -103,7 +106,7 @@ def task2(model, tokenizer, device, args, times):
                 "input": "",
                 "answer": answer2
             }
-            append_to_jsonl(temp, "../../../data/susgen/tcfd_qa/cache/diverse_v2_+.jsonl")
+            append_to_jsonl(temp, "../../../data/susgen/tcfd_qa/cache/diverse_v2_+4.jsonl")
             final.append(temp)
             print("="*50)
             state += 1
@@ -111,8 +114,7 @@ def task2(model, tokenizer, device, args, times):
             print("="*50)
             data.append(temp)
     
-    save_json(final, "../../../data/susgen/tcfd_qa/tcfd_qa_v2_+.json")
-    save_json(data, "../../../data/susgen/tcfd_qa/tcfd_qa_v2.json")
+    save_json(final, "../../../data/susgen/tcfd_qa/tcfd_qa_v2_+4.json")
 
 def main():
     model, tokenizer, device, _ = load_model()
@@ -125,11 +127,9 @@ def main():
         "num_return_sequences": 1
     }
 
-    # start processing
     # task1(model, tokenizer, device, args)
-    denoise_text()
-    time.sleep(30)
-    task2(model, tokenizer, device, args, 1)
+    # denoise_text()
+    task2(model, tokenizer, device, args, 2)
 
 if __name__ == "__main__":
     main()
