272e367cfab38a7cef5cc2c6cd76c2964ec412b0
Shawn Hsiang <133613082+ShowHsiang@users.noreply.github.com>
2024-05-19 15:01:58 +0800

    Update esg_sentiment_convert.py

--- data/susgen/add_data/esg_sentiment/esg_sentiment_convert.py
+++ data/susgen/add_data/esg_sentiment/esg_sentiment_convert.py
@@ -43,3 +43,53 @@ with open('esg_sentiment_test.json', 'w', encoding='utf-8') as json_file:
     json.dump(data_list1, json_file, indent=4, ensure_ascii=False)
 
 print("JSON testing file has been created successfully.")
+
+# Update datasets
+with open('esg_sentiment_train.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+new_data = []
+
+for item in data:
+    outputs = item['output'].split(', ')
+    base_instruction = "You are an expert in the field of ESG and help to determine the sentiment category of a given text in"
+    
+    for output in outputs:
+        domain, sentiment = output.split() 
+        new_instruction = f"{base_instruction} {domain}."
+        new_item = {
+            "instruction": new_instruction,
+            "input": item['input'],
+            "output": sentiment
+        }
+        new_data.append(new_item)
+
+with open('esg_sentiment_train_v2.json', 'w', encoding='utf-8') as file:
+    json.dump(new_data, file, indent=4)
+
+print("Modified Training JSON file has been created successfully.")
+
+
+with open('esg_sentiment_test.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+new_data = []
+
+for item in data:
+    outputs = item['output'].split(', ')
+    base_instruction = "You are an expert in the field of ESG and help to determine the sentiment category of a given text in"
+    
+    for output in outputs:
+        domain, sentiment = output.split() 
+        new_instruction = f"{base_instruction} {domain}."
+        new_item = {
+            "instruction": new_instruction,
+            "input": item['input'],
+            "output": sentiment
+        }
+        new_data.append(new_item)
+
+with open('esg_sentiment_test_v2.json', 'w', encoding='utf-8') as file:
+    json.dump(new_data, file, indent=4)
+
+print("Modified Testing JSON file has been created successfully.")
