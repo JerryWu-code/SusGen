75e3d360b2587e953c7f3c0039c86b2107bde87c
Jerry <953855668@qq.com>
2024-06-01 16:46:13 +0800

    Refactor the whole repository and modify the .gitignore.

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-# Author: "WHATX" -- Wu Qilong
-# Institute: National University of Singapore, A Star IHPC
-# Description: Serve as the main entrance of the src.
-
-#############################################################################
--- src/llms/llama3/inference.sh
+++
@@ -1,5 +0,0 @@
-torchrun example_chat_completion.py \
-    --ckpt_dir ../../../ckpts/Meta-Llama-3-8B-Instruct/ \
-    --tokenizer_path ../../../ckpts/Meta-Llama-3-8B-Instruct/tokenizer.model \
-    --max_seq_len 256 \
-    --max_batch_size 6
